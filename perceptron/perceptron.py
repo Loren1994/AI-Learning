22c660327ae25d2f184d22c970d664ec9ea73e3c
loren1994 <dayan805@163.com>
2018-08-20 15:07:15 +0800

    init

---
+++ perceptron/perceptron.py
@@ -0,0 +1,51 @@
+# AI - 感知器
+
+# 3和2差异
+# lambda a, b: a + b, map(lambda (x, w): x * w, zip(input_vec, self.weights))
+# 应改为 [x * w for x, w in zip(input_vec, self.weights)] // lambda (x,w):x*w => lambda x:x[0]*x[1]
+# 打印map => print(list(map))
+# reduce内置函数需导入from functools import reduce
+
+from functools import reduce
+
+
+class perceptron(object):
+    # 初始化感知器,设置参数个数,激活函数
+    def __init__(self, input_num, activator):
+        # 激活函数
+        self.activator = activator
+        # 权重
+        self.weights = [0.0 for _ in range(input_num)]
+        # 偏置项
+        self.bias = 0.0
+
+    # 学习到的权重、偏置项
+    def __str__(self):
+        return 'weights\t:%s\nbias\t:%f\n' % (list(self.weights), self.bias)
+
+    # 输入向量,输出感知器结果
+    # 把input_vec[x1,x2,x3...]和weights[w1,w2,w3,...]打包在一起
+    # 变成[(x1,w1),(x2,w2),(x3,w3),...]
+    # 然后利用map函数计算[x1*w1, x2*w2, x3*w3]
+    # 最后利用reduce求和
+    def predict(self, input_vec):
+        return self.activator(
+            reduce(lambda a, b: a + b,
+                   [x * w for x, w in zip(input_vec, self.weights)], 0.0) + self.bias)
+
+    # 输入训练数据：一组向量、与每个向量对应的label；以及训练轮数、学习率
+    def train(self, input_vecs, labels, iteration, rate):
+        for i in range(iteration):
+            self._one_train(input_vecs, labels, rate)
+
+    # 一次训练,把所有的训练数据过一遍
+    def _one_train(self, input_vecs, labels, rate):
+        samples = zip(input_vecs, labels)
+        for (input_vec, label) in samples:
+            output = self.predict(input_vec)
+            self._update_weight(output, input_vec, label, rate)
+
+    def _update_weight(self, output, input_vec, label, rate):
+        delta = label - output
+        self.weights = [w + rate * delta * x for x, w in zip(input_vec, self.weights)]
+        self.bias += rate * delta
---
+++ perceptron/train_perceptron.py
@@ -0,0 +1,34 @@
+# 感知器训练测试
+
+# 定义激活函数
+import perceptron
+
+
+def f(x: int):
+    return 1 if x > 0 else 0
+
+
+# 基于and真值表构建训练数据
+def get_training_data_sets():
+    input_vecs = [[1, 1], [0, 0], [1, 0], [0, 1]]
+    labels = [1, 0, 0, 0]
+    return input_vecs, labels
+
+
+# 使用and真值表训练感知器
+def train_and_perceptron():
+    p = perceptron.perceptron(2, f)
+    input_vecs, labels = get_training_data_sets()
+    p.train(input_vecs, labels, 10, 0.1)
+    return p  # 训练好的感知器
+
+
+if __name__ == '__main__':
+    and_perceptron = train_and_perceptron()
+    # 打印训练获得的权重
+    print(and_perceptron)
+    # 测试
+    print('1 and 1 = %d' % and_perceptron.predict([1, 1]))
+    print('0 and 0 = %d' % and_perceptron.predict([0, 0]))
+    print('1 and 0 = %d' % and_perceptron.predict([1, 0]))
+    print('0 and 1 = %d' % and_perceptron.predict([0, 1]))
